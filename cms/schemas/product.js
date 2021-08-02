785a4c1f2830c9f31be056096e5ee28301acad27
yerowyn <yerowyn@gmail.com>
2021-08-02 07:02:25 +0600

    init

---
+++ cms/schemas/product.js
@@ -0,0 +1,25 @@
+export default {
+  name: "product",
+  title: "Product",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+    },
+    {
+      name: "description",
+      title: "Description",
+      type: "text",
+    },
+    {
+      name: "image",
+      title: "Product Image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    },
+  ],
+};
---
+++ cms/schemas/theme.js
@@ -0,0 +1,22 @@
+export default {
+  name: "theme",
+  title: "Theme",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+    },
+    {
+      name: "description",
+      title: "Description",
+      type: "text",
+    },
+    {
+      name: "favoriteColor",
+      title: "Favorite color",
+      type: "color",
+    },
+  ],
+};
